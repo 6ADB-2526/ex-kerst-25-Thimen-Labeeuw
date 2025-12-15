bb34b8a3acf0f097a44191fd5085c7b363cf6b93
Thimen Labeeuw <Thimen.Labeeuw@student.tsaam.be>
2025-12-15 10:14:30 +0100

    v2

--- client/js/index.js
+++ client/js/index.js
@@ -2,6 +2,12 @@ const loginInput = document.getElementById("username")
 const passwordInput = document.getElementById("password")
 const submitBtn = document.getElementById("submit")
 
+//changepassword
+const currentPass = document.getElementById("currentPSW")
+const newPass = document.getElementById("newPSW")
+const newPassConfirm = document.getElementById("confirmPSW")
+const changePSWbtn = document.getElementById("changePSW")
+
 //sections
 sectionLogin = document.getElementById("beforLogin")
 sectionAfter = document.getElementById("afterLogin")
@@ -13,8 +19,8 @@ submitBtn.addEventListener("click",() => {
         "Content-Type": "application/json",
         },
         body: JSON.stringify({
-        "login": "els.dotjes",
-        "password": "EDotjes_!884"
+        "login": `${loginInput.value}`,
+        "password": `${passwordInput.value}`
         }),
     };
     fetch("https://ex-kerst-2025.onrender.com/user", options)
@@ -25,11 +31,23 @@ submitBtn.addEventListener("click",() => {
         .then(allUsers => {
             userToCheck = allUsers.filter(user => user.id == userID.info)
             console.log(userToCheck);
-            if (userToCheck.login == loginInput.value && userToCheck.password == passwordInput.value){
+            if (userToCheck[0].login == loginInput.value && userToCheck[0].password == passwordInput.value){
                 //oveschakelen
+                console.log("gelukt");
+                
+                
                 sectionLogin.style.display = "none"
                 sectionAfter.style.display = "block"
             }
+            else{
+                console.log("niet gelukt");
+                
+            }
+            
         })
     })
 })
+
+
+
+
